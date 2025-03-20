1412fe3fcd5ddfb6b241c62c1491c0382bdab3b7
Vishwajeet Kumar <vishwa843113@gmail.com>
2025-03-20 16:20:28 +0530

    Update SetMatrixZeros73.py

--- 19March/SetMatrixZeros73.py
+++ 19March/SetMatrixZeros73.py
@@ -20,5 +20,21 @@ def setMatrixZeros(matrix):
         for i in range(len(matrix)):
             matrix[i][0] = 0
     return matrix
-matrix = [[1,1,1],[1,0,1],[1,1,1]]
+
+    # for i in range(len(matrix)):
+    #     for j in range(len(matrix[0])):
+    #         if matrix[i][j] == 0:
+    #             for k in range(len(matrix[0])):
+    #                 if matrix[i][k] != 0:
+    #                     matrix[i][k] = -1
+    #             for k in range(len(matrix)):
+    #                 if matrix[k][j] != 0:
+    #                     matrix[k][j] = -1
+    # for i in range(len(matrix)):
+    #     for j in range(len(matrix[0])):
+    #         if matrix[i][j] == -1:
+    #             matrix[i][j] = 0
+    # return matrix
+
+matrix = [[1,1,1],[1,0,1],[1,1,2]]
 print(setMatrixZeros(matrix))
