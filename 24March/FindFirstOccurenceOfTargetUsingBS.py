795ebb9451af2c6bfe8cb7f1819bd2f7afa6fc24
Vishwajeet Kumar <vishwa843113@gmail.com>
2025-03-25 15:45:45 +0530

    Update FindFirstOccurenceOfTargetUsingBS.py

--- 24March/FindFirstOccurenceOfTargetUsingBS.py
+++ 24March/FindFirstOccurenceOfTargetUsingBS.py
@@ -2,14 +2,17 @@ def first_Binary_search(nums,target):
         ans=-1
         left=0
         right=len(nums)-1
+        mid = left + (right - left)// 2
         while left <= right:
-            mid=(left + right)//2
+            
             if nums[mid]==target:
                 ans = mid
                 right= mid - 1
-            elif nums[mid] > target:
+          
+            if nums[mid] > target:
                 right = mid - 1
             else:
                 left =mid + 1
-        return ans
+            mid = left + (right - left)//2
+        return ans-1
 print(first_Binary_search([5,7,7,8,8,10],8))
