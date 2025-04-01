0ded838034c0c79cbb9a649f1e11fb12a4fbe788
Vishwajeet Kumar <vishwa843113@gmail.com>
2025-04-01 16:25:54 +0530

    Create SearchIn2DMatrix74.py

---
+++ 01April/SearchIn2DMatrix74.py
@@ -0,0 +1,23 @@
+def searchIna2Dmatrix(matrix,target):
+    if not matrix:
+        return False
+    row = 0
+    col = len(matrix[0]) - 1
+    while row < len(matrix) and col >= 0:
+        if matrix[row][col] == target:
+            return True
+        elif matrix[row][col] > target :
+            col -= 1
+        else:
+            row += 1
+    return False
+
+    # for i in matrix:
+    #     if target in i:
+    #         return True
+    # return False
+matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+target = 34
+print(searchIna2Dmatrix(matrix,target))
+
+
