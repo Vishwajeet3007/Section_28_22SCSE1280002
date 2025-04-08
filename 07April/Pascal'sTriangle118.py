ff6f5e102b605b75805ce7f2c5e66443612b0290
Vishwajeet Kumar <vishwa843113@gmail.com>
2025-04-08 08:35:51 +0530

    Create Pascal'sTriangle118.py

---
+++ 07April/Pascal'sTriangle118.py
@@ -0,0 +1,10 @@
+def generate(numRows):
+    result = []
+    for i in range(numRows):
+        row = [1] * (i+1)
+        for j in range(1,i):
+            row[j] = result[i-1][j] + result[i-1][j-1]
+        result.append(row)
+    return result
+numRows = 5
+print(generate(numRows))
