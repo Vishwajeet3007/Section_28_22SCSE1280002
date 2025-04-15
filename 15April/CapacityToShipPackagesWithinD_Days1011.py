9a3bc1e266bc75634627130aa60daf38a196f431
Vishwajeet Kumar <vishwa843113@gmail.com>
2025-04-15 23:33:11 +0530

    Update CapacityToShipPackagesWithinD_Days1011.py

--- 15April/CapacityToShipPackagesWithinD_Days1011.py
+++ 15April/CapacityToShipPackagesWithinD_Days1011.py
@@ -1,2 +1,23 @@
 def maxCapacity(weights,days):
-    pass
+    min_cap = max(weights)
+    max_cap = sum(weights)
+    while min_cap < max_cap:
+        mid = min_cap + (max_cap - min_cap) // 2
+        day = 1
+        summ = 0
+        for weight in weights:
+            if summ + weight > mid:
+                day += 1
+                summ = 0
+            summ += weight
+        if day > days:
+            min_cap = mid + 1
+        else:
+            max_cap = mid
+    return min_cap
+weights = [1,2,3,4,5,6,7,8,9,10]
+days = 5
+print(maxCapacity(weights,days))
+weights = [3,2,2,4,1,4]
+days = 3
+print(maxCapacity(weights,days))
