4c55dcb4b10417e8dae9f1b6c3761476526ccfb5
Vishwajeet Kumar <vishwa843113@gmail.com>
2025-04-30 10:56:42 +0530

    Update 29Is_Palindrome.py

--- MTE Assignment/29Is_Palindrome.py
+++ MTE Assignment/29Is_Palindrome.py
@@ -1,12 +1,26 @@
 def is_palindrome(x):
-    if x < 0:
-        return False
-    original = x
-    reversed_num = 0
-    while x > 0:
-        digit = x % 10
-        reversed_num = reversed_num * 10 + digit
-        x = x // 10
-    return original == reversed_num
+    # if x < 0:
+    #     return False
+    # original = x
+    # reversed_num = 0
+    # while x > 0:
+    #     digit = x % 10
+    #     reversed_num = reversed_num * 10 + digit
+    #     x = x // 10
+    # return original == reversed_num
+
+# Using Two Pointers
+    x = str(x)
+    n = len(x)
+    left = 0
+    right = n  - 1
+    while left <= right:
+        if x[left] == x[right]:
+            left += 1
+            right -= 1
+        else:
+            return False
+    return True
+
 print(is_palindrome(121))
 print(is_palindrome(123))
