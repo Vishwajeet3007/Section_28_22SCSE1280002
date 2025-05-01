e74402025265377b404f6d192d4f016a336633e8
Vishwajeet Kumar <vishwa843113@gmail.com>
2025-05-01 09:18:04 +0530

    Update 40Maximum_frequency_Element.py

--- MTE Assignment/40Maximum_frequency_Element.py
+++ MTE Assignment/40Maximum_frequency_Element.py
@@ -4,6 +4,6 @@ def max_frequency_element(nums):
     max_element = max(freq,key=freq.get)
     max_freq = freq[max_element]
     #max_elem, max_freq = Counter(arr).most_common(1)[0]
-    return max_element
+    return max_freq
 arr = [1, 3, 2, 1, 4, 1, 3, 3, 3]
 print(max_frequency_element(arr))
